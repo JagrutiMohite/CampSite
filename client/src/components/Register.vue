a379fa85ecc1e125f058007e2010b70769d08db2
JagrutiMohite <mohitejagruti5@gmail.com>
2019-03-13 12:42:20 -0400

    Register with vue

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,11 @@
 <template>
- <div>
-     <h1>Register</h1>
+<v-layout>
+  <v-flex xs6 offset-xs3>
+    <div class="white elevation-2">
+      <v-toolbar flat dense class="green darken-1" dark>
+      <v-toolbar-title>Register</v-toolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
      <input
         type="first name"
         name="first name"
@@ -31,8 +36,13 @@
         v-model="password"
         placeholder="password" />
         <br>
-        <button @click="register">Register</button>
- </div>
+        <div class="error" v-html="error" />
+        <br>
+        <v-btn class="green darken-1" @click="register">Register</v-btn>
+        </div>
+      </div>
+  </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -44,19 +54,23 @@ export default {
       LastName: '',
       Phone: '',
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        FirstName: this.FirstName,
-        LastName: this.LastName,
-        Phone: this.Phone,
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          FirstName: this.FirstName,
+          LastName: this.LastName,
+          Phone: this.Phone,
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -64,4 +78,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color: red;
+}
 </style>
